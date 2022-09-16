a25be04633a26e3fcf76df82389c6425f09288bc
eih1 <93357783+eih1@users.noreply.github.com>
2022-09-16 18:27:24 +0100

    Create html.py

---
+++ html.py
@@ -0,0 +1,18 @@
+name = input("Name of `HTML` file: ")
+print("Creating file...")
+
+try: 
+    file = open('index.html', 'x')
+    file.write("""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+</body>
+</html>""")
+    file.close()
+except:
+    print('A file with name \'index.html\' already exists in this directory.')
